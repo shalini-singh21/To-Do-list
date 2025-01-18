7471f57fc391a36aa25f2985afbf15acdc9c86c8
shalini singh <shalu.singh13042gmail.com>
2025-01-18 16:57:55 +0530

    hhiii

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 
 import './App.css';
-import Comment from './component/Comment';
-import { useEffect } from 'react';
+// import Comment from './component/Comment';
+// import { useEffect } from 'react';
 import React from 'react';
-import {getPosts} from "./component/api";
+// import {getPosts} from "./component/api";
+import Input from"./component/Input"
 function App() {
-useEffect(()=>{
-  getPosts().then((posts)=>console.log(posts));
-} ,[]);
+// useEffect(()=>{
+//   getPosts().then((posts)=>console.log(posts));
+// } ,[]);
 
   return (
     <>
+    <Input/>
    
-     <Comment />
+     {/* <Comment /> */}
   
      
    
